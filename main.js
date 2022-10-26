7a8ddf4ac953443f2d61795c03924afb66ef632a
nataleya2010 <gnwr2010@gmail.com>
2022-10-26 15:26:06 +0300

    feat: change functions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDb = {
     count: numberOfFilms,
     movies: {},
@@ -7,12 +7,22 @@ const personalMovieDb = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов',''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов',''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+for (let i=1; i<=2; i++){
+    const a = prompt('Один из последних просмотренных фильмов',''),
+    b = prompt('На сколько оцените его?', '');
+    if(a != null && b != null && a != '' && b != '' && a.length<=50){
+        personalMovieDb.movies[a] = b;
+    }else{
+        i--;
+    }
+}
+if(numberOfFilms<10){
+    console.log('вы посмотрели довольно мало фильмов!');
+}else if(numberOfFilms>=10 && numberOfFilms<30){
+    console.log('вы классичекский зритель!');
+}else if(numberOfFilms>=30){
+    console.log('вы настоящий киноман!');
+}
 
 console.log(personalMovieDb);
